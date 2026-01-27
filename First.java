4c188ce0e5bc74b71bb664581224f0a5e8b3ba7e
Muhammad Rafi Alam <rafialamwork786@gmail.com>
2026-01-27 20:13:30 +0600

    Add files via upload
    my first JAVA code

---
+++ First.java
@@ -0,0 +1,17 @@
+public class First {
+   public static void main (String[] args)  {
+
+       // this is a comment and my first java program 
+       /* this 
+           is 
+           a
+           multiline comment */
+     
+           System.out.println("My name is Rafi");
+           System.out.println("I am in NWU ");
+           
+           System.out.print("\nMy name is Rafi \n");
+           System.out.print("I am in NWU ");
+                      
+     }
+} 
