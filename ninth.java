e423409448fbcc28860d1a85efcf0a92ce7dc19a
Muhammad Rafi Alam <rafialamwork786@gmail.com>
2026-01-31 20:13:18 +0600

    Add files via upload

---
+++ ninth.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ninth {
+    public static void main(String[] args) {
+        
+        //string system in JAVA
+        Scanner scanner = new Scanner(System.in);
+
+        String name = "Rafi alam";
+
+        int length = name.length(); // with this funciton you can print the length of any string
+
+        System.out.println(length);
+
+        scanner.close();
+    }
+}
