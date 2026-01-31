6f77b7b434437cfa900775bd01f401c6dd0442da
Muhammad Rafi Alam <rafialamwork786@gmail.com>
2026-01-31 20:11:37 +0600

    Add files via upload

---
+++ Sixth.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Sixth {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int number1;
+        int number2;
+        int number3;
+
+        number1 = random.nextInt(1,7);
+        number2 = random.nextInt(1,7);
+        number3 = random.nextInt(1,7);
+        
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+
+        scanner.close();
+    }
+}
