aab3429d30f1cd22678fb04674e80b808f78f87a
Muhammad Rafi Alam <rafialamwork786@gmail.com>
2026-02-11 23:21:01 +0600

    Implement Merge Sort algorithm in MergeSort.java

---
+++ MergeSort.java
@@ -0,0 +1,49 @@
+public class MergeSort {
+    public static void sort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+
+    private static void merge(int[] arr, int l, int m, int r) {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        // Create temp arrays
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        // Copy data to temp arrays
+        for (int i = 0; i < n1; ++i) L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j) R[j] = arr[m + 1 + j];
+
+        // Merge the temp arrays
+        int i = 0, j = 0, k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i++];
+            } else {
+                arr[k] = R[j++];
+            }
+            k++;
+        }
+
+        // Copy remaining elements if any
+        while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6, 7};
+        sort(arr, 0, arr.length - 1);
+        System.out.println("Sorted: " + java.util.Arrays.toString(arr));
+    }
+}
