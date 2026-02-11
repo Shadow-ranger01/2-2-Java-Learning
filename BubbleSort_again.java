673c7d15a6a840558f8a55a100a131c2399b5329
Muhammad Rafi Alam <rafialamwork786@gmail.com>
2026-02-11 23:24:40 +0600

    Implement Bubble Sort algorithm in Java

---
+++ BubbleSort_again.java
@@ -0,0 +1,28 @@
+public class BubbleSort {
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            // Last i elements are already in place
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // If no two elements were swapped in the inner loop, break
+            if (!swapped) break;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] data = {64, 34, 25, 12, 22, 11, 90};
+        sort(data);
+        System.out.println("Sorted Array: " + java.util.Arrays.toString(data));
+    }
+}
